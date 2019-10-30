1a70973d3f8e6b943ee865df4e5681d6219e26fd
Alec Flowers <alec.flowers00@gmail.com>
2019-10-29 21:22:39 -0700

    feat: Build ML model to classify disaster messages

--- process_data.py
+++ process_data.py
@@ -1,35 +1,85 @@
+import sys
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 
-def etl_pipeline():
+def load_data(message_path,categories_path):
     """
-    Reads in messages and categories csv. Merges files, cleans data and writes output to SQL Lite to be
-    used in a ML model.
+    Load message & category data and merge on commmon id.
+
+    Output: 
+    df (dataframe) : merged message & category data
     """
-    messages_df = pd.read_csv('messages.csv')
-    categories_df = pd.read_csv('categories.csv')
+    messages_df = pd.read_csv(message_path)
+    categories_df = pd.read_csv(categories_path)
 
     df = messages_df.merge(categories_df,on='id')
 
-    categories = df.categories.str.split(pat=';',expand = True)
+    return df
 
-    #create column names for labeled data
+def etl_pipeline(df):
+    """
+    Cleans merged data by naming columns, extracting binary variables, and taking out duplicates.
+    
+    Output:
+    df (dataframe) : Cleaned dataframe
+    """
+
+    #Pull column names from data and label columns
+    categories = df.categories.str.split(pat=';',expand = True)
     title_row = categories.iloc[0]
     category_colnames = title_row.apply(lambda x : x.split('-')[0]).tolist()
     categories.columns = category_colnames
 
+    #turn string data into ints 
     for column in categories:
         categories[column] = categories[column].str.split('-').str.get(1)
         categories[column] = categories[column].astype(int)
 
+    #drop unecessary columns, duplicates, and correct non-binary data
     df = df.drop('categories',axis=1)
     df = pd.concat([df,categories],axis=1)
     df = df.drop_duplicates()
     df.loc[df.related == 2,'related'] = 1
     df = df.drop('child_alone',axis=1)
 
-    engine = create_engine('sqlite:///DisasterResponseDatabase.db')
+    return df
+
+def save_data(df,database_filepath):
+    """
+    Create SQL Lite database to store the cleaned data.
+    Output:
+    sqlite database
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
     df.to_sql('cleaned_disaster_data', engine, index=False)
 
-etl_pipeline()
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = etl_pipeline(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ train_classifier.py
@@ -0,0 +1,205 @@
+import sys
+
+from sqlalchemy import create_engine
+
+import re
+import numpy as np
+import pandas as pd
+
+import nltk
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+from sklearn.metrics import confusion_matrix, classification_report,roc_curve, hamming_loss, accuracy_score, make_scorer, recall_score,precision_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.externals import joblib
+
+
+def load_data(database_filepath):
+    """
+    Load cleaned data from SQL Lite database and break it up into X (data) and y (labels).
+
+    Output:
+    X (list) : Message data in string format
+    y (dataframe) : Classification labels for the message data
+    y.columns (list) : List of column names
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    conn = engine.connect()
+    df = pd.read_sql_query(""" Select * FROM cleaned_disaster_data""",conn)
+    X = df.message.values
+    y = df.drop(['id','message','original','genre'],axis=1)
+    return X,y,y.columns
+
+def tokenize(text):
+    """
+    Tokenize text, take out stop words, lemmatize text 
+
+    Input:
+    text (str)
+
+    Output:
+    lemmed (list) : List of strings 
+    """
+
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    words = word_tokenize(text)
+    no_stop_words = [w for w in words if w not in stopwords.words("english")]
+    lemmed_word_list = [WordNetLemmatizer().lemmatize(w).lower().strip() for w in no_stop_words]
+    return lemmed_word_list
+
+class get_first_verb(BaseEstimator,TransformerMixin):
+    def starting_verb(self,text):
+        """
+        Return true if first word is a verb, otherwise return false. 
+        """
+        sent_list = sent_tokenize(text)
+        
+        for sentence in sent_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            try:
+                first_word, first_tag = pos_tags[0]
+            except:
+                return False
+                continue
+            if first_tag in ['VB', 'VBP']:
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        """
+        Function from baseclass. Fits data
+        """
+        return self
+
+    def transform(self, X):
+        """
+        Function from baseclass. Transforms data
+        """
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)    
+
+class get_char_num(BaseEstimator,TransformerMixin):
+    def count_char(self,text):
+        """
+        Counts number of characters
+        """
+        return len(text)
+    def fit(self,X,y=None):
+        """
+        Function from baseclass. Fits data
+        """
+        return self
+    def transform(self,X):
+        """
+        Function from baseclass. Transforms data
+        """
+        X_count = pd.Series(X).apply(self.count_char)
+        return pd.DataFrame(X_count)
+
+def build_model():
+    """
+    Define pipeline to 
+        1) transform sentences into features that we can train model with.
+        2) Create Random Forest Model to train 
+    Set grid search parameters and scorers to optimize model.
+
+    Output:
+    cv (GridSearchCV object) : model ready to be trained
+    """
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', get_first_verb()),
+            ('text_length',get_char_num())
+        ])),
+
+        ('clf', RandomForestClassifier())
+    ])
+    parameters = {
+        #'clf__min_samples_split': [3, 5, 10], 
+        'clf__n_estimators' : [10],      #[100, 300],
+        'clf__max_depth': [5]             #[3, 5, 15, 25],
+        #'clf__max_features': [3, 5, 10, 20]
+        }
+
+    scorers = {
+        'precision_score': make_scorer(precision_score,average = 'micro'),
+        'recall_score': make_scorer(recall_score,average = 'micro'),
+        'accuracy_score': make_scorer(accuracy_score)
+        }
+
+    cv = GridSearchCV(pipeline,parameters,scoring = scorers,refit = 'recall_score')
+
+    return cv
+
+def evaluate_model(model,X_test,y_true,category_names):
+    """
+    Predict test data labels. Print out various model evaluation metrics
+    comparing test and actual labels. 
+    """
+    y_pred = pd.DataFrame(model.predict(X_test))
+
+    print('Classification Report')
+    print(classification_report(y_true,y_pred,target_names = category_names))
+    print('Model Accuracy: {}'.format(accuracy_score(y_true,y_pred)))
+    print('Model Hamming Loss: {}'.format(hamming_loss(y_true,y_pred)))
+    
+    #Print out classification report for every class we are trying to predict
+    for name in category_names:
+        print(name)
+        print(classification_report(y_true[name],y_pred[name]))
+
+def save_model(model, model_filepath):
+    """
+    Use joblib to save snapshot of trained model
+    """
+    joblib.dump(model, model_filepath)
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
